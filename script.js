a0a7fd76320ef1e2f9948d504bc734858eb7e5d9
VSinha25 <71936568+VSinha25@users.noreply.github.com>
2023-12-28 01:39:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+//Prompt to select media stream, pass to video element, then play
+async function selectMediaStream() {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+    } catch(error){
+        console.log('Oops, error is here: ', error);
+        //Catching error here
+    }
+}
+
+button.addEventListener('click', async () => {
+    //Disable button
+    button.disabled = true;
+    // Start Picture in Picture
+    await videoElement.requestPictureInPicture();
+    // Reset button 
+    button.disabled = false;
+});
+
+//On Load
+selectMediaStream();
